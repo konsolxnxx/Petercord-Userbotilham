a74f132deab420e4756e251d85cab171d42922e2
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-03-11 20:12:47 +0700

    Update lordae.py

--- userbot/modules/lordae.py
+++ userbot/modules/lordae.py
@@ -1,5 +1,5 @@
 # Based Plugins
-# Ported for Lord-Userbot By liualvinas/Alvin
+# Ported for Petercord-Userbot By bismillahselaluadaa/ILham
 
 from telethon import events
 from userbot.events import register
