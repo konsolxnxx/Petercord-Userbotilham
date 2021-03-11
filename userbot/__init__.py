ccf531749266ed8ed15268d383ed48228bbbd79c
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-03-11 10:12:22 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -447,7 +447,7 @@ with bot:
                 # https://t.me/TelethonChat/115200
                 await event.edit(buttons=buttons)
             else:
-                reply_pop_up_alert = f"Harap Deploy Lord Userbot Anda Sendiri, Jangan Menggunakan Milik Lord {ALIVE_NAME} ツ"
+                reply_pop_up_alert = f"Harap Deploy Petercord Userbot Anda Sendiri, Jangan Menggunakan Milik Lord {ALIVE_NAME} ツ"
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
         @tgbot.on(
