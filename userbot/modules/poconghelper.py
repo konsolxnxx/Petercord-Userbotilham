378d6feb174f2fa893a0dd2dc0bd0370c88ea8e5
konsolxnxx <81410308+konsolxnxx@users.noreply.github.com>
2021-03-26 16:52:54 +0700

    Update poconghelper.py

--- userbot/modules/poconghelper.py
+++ userbot/modules/poconghelper.py
@@ -12,16 +12,16 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def usit(e):
     await e.edit(
         f"**Hai tuan {DEFAULTUSER} Kalo Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Telegram](t.me/bismillahselaluadaa)"
-        "\n[Repo](https://github.com/ilham77mansiz/tuan-Userbotilham)"
-        "\n[Instagram](Instagram.com/imansiezz_)")
+        "\n[Telegram](t.me/PocongOnlen)"
+        "\n[Repo](https://github.com/konsolxnxx/PocongUserbot)"
+        "\n[Instagram](pornhub.com)")
 
 
 @register(outgoing=True, pattern="^.pocongvar$")
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/ilham77mansiz/Lord-Userbot/Lord-Userbot/varshelper.txt)")
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/konsolxnxx/PocongUserbot/pocong/varshelper.txt)")
 
 
 CMD_HELP.update({
