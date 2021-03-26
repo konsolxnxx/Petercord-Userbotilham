efe428c2e0ac9704f2c88043b49fdd655deb26c4
konsolxnxx <81410308+konsolxnxx@users.noreply.github.com>
2021-03-26 18:12:09 +0700

    Update CI_Test_Script.sh

--- init/CI_Test_Script.sh
+++ init/CI_Test_Script.sh
@@ -21,5 +21,5 @@ git remote set-url origin https://${GH_USERNAME}:${GH_PERSONAL_TOKEN}@github.com
 git fetch
 git checkout staging
 git pull origin staging
-git push --force origin staging: Petercord User
-tg_sendinfo "<code>I have merged all commits from staging into Lord-Userbot</code>"
+git push --force origin staging: pocong
+tg_sendinfo "<code>I have merged all commits from staging into Pocong Userbot</code>"
