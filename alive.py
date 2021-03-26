2bc7084bab3b408389f04008f8fa326bcc376691
konsolxnxx <81410308+konsolxnxx@users.noreply.github.com>
2021-03-26 15:18:14 +0700

    Delete alive.py

--- alive.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Filters
-from userbot import bot
-
-@bot.on_message(Filters.regex("^.alive"))
-def amialivedad(event):
-    chat = event.chat.id 
-    message = " Petercord User :)"
-    bot.edit_message_text(chat_id=chat, message_id="me", text=message)
